d4539c111c11fc10b8454f3daa453aad6763f93f
Tenzin Kalden <tkalden61@gmail.com>
2024-11-14 11:10:43 +0530

    first push

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'class_id', 'section_id'];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+}
